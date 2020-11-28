830805ec65297c864d4c336764c76e9d5cb77479
Drayfus7033 <tudor703344@gmail.com>
2020-11-28 15:51:10 +0100

    added stuff

---
+++ mainscript.js
@@ -0,0 +1,17 @@
+﻿$(window).ready(function () {
+
+    var wHeight = $(window).height();
+
+    $('.slide')
+        .scrollie({
+            scrollOffset: -300,
+            scrollingInView: function (elem) {
+                console.log('change colour');
+                var bgColor = elem.data('background');
+
+                $('body').css('background-color', bgColor);
+
+            }
+        });
+
+});
